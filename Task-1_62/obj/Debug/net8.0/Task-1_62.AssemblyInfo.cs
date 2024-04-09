6ab84e9e2687dcc4f4c139d9f47d570cc051ad00
ladynazgool <ladynazgool@gmail.com>
2024-04-09 15:09:29 +0400

    Task 6-67 finished

--- Task-1_62/obj/Debug/net8.0/Task-1_62.AssemblyInfo.cs
+++ Task-1_62/obj/Debug/net8.0/Task-1_62.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task-1_62")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9cacacb718dd19bf5d2658d7790c1335e881d91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task-1_62")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task-1_62")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task-6_67/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать рекурсивную функцию определения суммы цифр числа N, введённого с клавиатуры
+
+// int Function (int n)
+// {
+//     if (n < 10) return n;
+//     else return Function(n/10) + n%10;
+// }
+// int N = 5647;
+// Console.WriteLine($"Сумма цифр числа {N} равна {Function(N)}");
+
+
+// int SumNumber(int n)
+// {
+//     if (n == 0)
+//         return 0;
+//     return n%10 + SumNumber(n/10);
+// }
+
+
+
+// Использование тернарного оператора, получается еще короче
+
+int SumNumber (int n)
+{
+    return n == 0 ? 0 : n%10 + SumNumber(n/10); // синтаксис этой фигни: значение - вопрос - значение, если условие истинно - двоеточие - значение, если условие ложно
+}
+
+Console.Write("Enter a number: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Sum of digits: {SumNumber(n)}");
+
+// Если в функции содержится только один оператор return, то можно все записать в 1 строку:
+
+// int SumNumber (int n) => n == 0 ? 0 : n % 10 + SumNumber(n / 10);
